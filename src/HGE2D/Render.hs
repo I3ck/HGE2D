92fa083a223fac42bf13d468e81b41d7fadd68a2
Martin Buck <buckmartin@buckmartin.de>
2016-02-02 20:53:28 +0100

    better formatting of RenderInstruction instance

--- src/HGE2D/Render.hs
+++ src/HGE2D/Render.hs
@@ -7,16 +7,23 @@ import HGE2D.ShapeFactory
 import Graphics.Rendering.OpenGL as GL
 import Graphics.UI.GLUT
 
+--------------------------------------------------------------------------------
+
 instance GlRender RenderInstruction where
     glRender renderInstruction = case renderInstruction of
-            (RenderNothing)             -> return ()
-            (RenderText text)           -> currentRasterPosition $= Vertex4 0 0 0 1 >> renderString TimesRoman24 text --- TODO font as option?
-            (RenderLineStrip shape w)   -> lineWidth $= w >> (renderPrimitive LineStrip $ mapM_ vertex shape)
-            (RenderTriangle shape)      -> renderPrimitive Triangles $ mapM_ vertex shape
-            (RenderLineLoop shape w)    -> lineWidth $= w >> (renderPrimitive LineLoop $ mapM_ vertex shape)
-            (RenderTranslate w h)       -> translate2 w h
-            (RenderRotate rad)          -> rotate2 rad
-            (RenderColorize rgb)        -> colorRGB rgb
-            (RenderColorizeAlpha rgba)  -> colorRGBA rgba
-            (RenderPreserve instrs)     -> preservingMatrix (mapM_ glRender instrs)
-            (RenderMany instrs)         -> mapM_ glRender instrs
+        RenderNothing            -> return ()
+        RenderText text          -> do currentRasterPosition $= Vertex4 0 0 0 1 
+                                       renderString TimesRoman24 text --- TODO font as option?
+
+        RenderLineStrip shape w  -> do lineWidth $= w 
+                                       (renderPrimitive LineStrip $ mapM_ vertex shape)
+
+        RenderTriangle shape     -> renderPrimitive Triangles $ mapM_ vertex shape
+        RenderLineLoop shape w   -> do lineWidth $= w 
+                                       (renderPrimitive LineLoop $ mapM_ vertex shape)
+        RenderTranslate w h      -> translate2 w h
+        RenderRotate rad         -> rotate2 rad
+        RenderColorize rgb       -> colorRGB rgb
+        RenderColorizeAlpha rgba -> colorRGBA rgba
+        RenderPreserve instrs    -> preservingMatrix (mapM_ glRender instrs)
+        RenderMany instrs        -> mapM_ glRender instrs
