4893917f27e117c6793548ac1537a7d44c7450ee
Martin Buck <buckmartin@buckmartin.de>
2016-02-05 16:56:30 +0100

    moved collision code to own file

---
+++ src/HGE2D/Collision.hs
@@ -0,0 +1,61 @@
+module HGE2D.Collision where
+
+import HGE2D.Types
+import HGE2D.Datas
+import HGE2D.Classes
+
+doCollideBB :: (HasBoundingBox a, HasBoundingBox b) => a -> b -> Bool
+doCollideBB hasBB1 hasBB2 = not $   (bb2Left   > bb1Right)
+                                 || (bb2Right  < bb1Left)
+                                 || (bb2Top    > bb1Bottom)
+                                 || (bb2Bottom < bb1Top)
+  where
+    bb1Top    = realY $ bbMin $ bb1
+    bb1Right  = realX $ bbMax $ bb1
+    bb1Bottom = realY $ bbMax $ bb1
+    bb1Left   = realX $ bbMin $ bb1
+
+    bb2Top    = realY $ bbMin $ bb2
+    bb2Right  = realX $ bbMax $ bb2
+    bb2Bottom = realY $ bbMax $ bb2
+    bb2Left   = realX $ bbMin $ bb2
+
+    bb1 = getBB hasBB1
+    bb2 = getBB hasBB2
+
+---TODO cleanup, align remove redundant $
+doOverlapBB :: (HasBoundingBox a, HasBoundingBox b) => a -> b -> Bool
+doOverlapBB hasBB1 hasBB2 = (isInsideBB hasBB1 hasBB2) || (isInsideBB hasBB2 hasBB1)
+
+isInsideBB :: (HasBoundingBox a, HasBoundingBox b) => a -> b -> Bool
+isInsideBB hasBBIn hasBBOut =  (bbInTop    > bbOutTop)
+                            && (bbInBottom < bbOutBottom)
+                            && (bbInLeft   > bbOutLeft)
+                            && (bbInRight  < bbOutRight)
+  where
+      bbInTop     = realY $ bbMin $ bbIn
+      bbInRight   = realX $ bbMax $ bbIn
+      bbInBottom  = realY $ bbMax $ bbIn
+      bbInLeft    = realX $ bbMin $ bbIn
+
+      bbOutTop    = realY $ bbMin $ bbOut
+      bbOutRight  = realX $ bbMax $ bbOut
+      bbOutBottom = realY $ bbMax $ bbOut
+      bbOutLeft   = realX $ bbMin $ bbOut
+
+      bbIn        = getBB hasBBIn
+      bbOut       = getBB hasBBOut
+
+isInsideRP :: (HasBoundingBox a) => RealPosition -> a -> Bool
+isInsideRP pos hasBB =  (posX > bbLeft)
+                     && (posX < bbRight)
+                     && (posY > bbTop)
+                     && (posY < bbBottom)
+  where
+    posX     = realX pos
+    posY     = realY pos
+    bbTop    = realY $ bbMin $ bb
+    bbRight  = realX $ bbMax $ bb
+    bbBottom = realY $ bbMax $ bb
+    bbLeft   = realX $ bbMin $ bb
+    bb       = getBB hasBB
