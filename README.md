e82e33a6178c8530b710dddced5a9f42f987d7d0
Martin Buck <buckmartin@buckmartin.de>
2016-02-07 16:50:56 +0100

    added working first example

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 #HGE2D
 A game engine written in Haskell
 ##Version 0.0.2
+
+##Examples
+
+[Simple "Hello world"](src/examples/Example1.hs)
--- src/examples/Example1.hs
+++ src/examples/Example1.hs
@@ -2,47 +2,63 @@ module Main where
 
 import HGE2D.Types
 import HGE2D.Datas
+import HGE2D.Colors
 import HGE2D.Classes
 import HGE2D.Instances
 import HGE2D.ShapeFactory
 import HGE2D.Engine
 
+{- Very basic example showing a "Hello World"
+   First we are going to define our GameState and instances,
+   and finally run the engine
+-}
 --------------------------------------------------------------------------------
 
+--in here we are going to store all data of the game
 data GameState = GameState
-    { gsSize :: (Double, Double) -- current size
-    , time   :: Millisecond      -- current time
+    { gsSize :: (Double, Double) -- current size of the entire game in pixels
+    , time   :: Millisecond      -- current time of the game
     }
 
 --------------------------------------------------------------------------------
 
+--this instance shall always return the window title of your game
 instance HasTitle GameState where
     getTitle _ = "Welcome to Example1"
 
+--should return the current size of the game window
 instance HasSize GameState where
     getW gs = fst $ gsSize gs
     getH gs = snd $ gsSize gs
 
+--used to know and initalize the game's time
 instance HasTime GameState where
     getTime gs = time gs
     setTime ms gs = gs { time = ms }
 
+--our first example is static, therefore we just gonna leave the game as-is
 instance Dynamic GameState where
-    moveInTime ms gs = gs --- TODO change state here (maybe simple counter for the example?)
+    moveInTime _ gs = gs
 
+--also we won't react to any input
 instance MouseInteract GameState where
-    click x y gs = gs
-    hover x y gs = gs
-    drag x y gs = gs
+    click _ _ gs = gs
+    hover _ _ gs = gs
+    drag _ _ gs = gs
 
+--enable the engine to pass window resizes to the game
 instance Resizeable GameState where
     resize w h gs = gs { gsSize = (realToFrac w, realToFrac h) }
     getSize gs = gsSize gs
 
+--how the state shall be rendered, in our example a simple "Hello World"
 instance GlInstructable GameState where
-    toGlInstruction gs = rectangle 0.3 0.3
-
-
+    toGlInstruction gs = RenderMany
+       [ RenderColorize colorWhite
+       , rectangle 0.3 0.3
+       , RenderColorize colorRed
+       , RenderText "Hello World"
+       ]
 
 --------------------------------------------------------------------------------
 
