8067b052b784261afcc1bd7151254c75056ae4c9
Martin Buck <buckmartin@buckmartin.de>
2016-06-22 17:38:44 +0200

    minor changes and version bump

--- src/HGE2D/Physical.hs
+++ src/HGE2D/Physical.hs
@@ -8,33 +8,35 @@ import HGE2D.Instances
 --------------------------------------------------------------------------------
 
 ---TODO not fully implemented
+---TODO rename
+---TODO rename class to HasPhysicalObject
 applyPhysics :: (IsPhysicalObject a) => Millisecond -> a -> a
 applyPhysics ms x = setPhys newPo x
-    where
-      newPo = (applyVel . applyAcc . applyRotVel . applyRotAcc . applyDrag . applyRotDrag) (getPhys x) ---TODO more consistent order acc > vel > abs
+  where
+    newPo = (applyVel . applyAcc . applyRotVel . applyRotAcc . applyDrag . applyRotDrag) (getPhys x) ---TODO more consistent order acc > vel > abs
 
-      applyVel po = moveBy (RealPosition dX dY) po ---TODO define more general for reuse
-        where
-          dX = (fromIntegral ms) * (velX $ physicalVel po)
-          dY = (fromIntegral ms) * (velY $ physicalVel po)
+    applyVel po = moveBy (RealPosition dX dY) po ---TODO define more general for reuse
+      where
+        dX = (fromIntegral ms) * (velX $ physicalVel po)
+        dY = (fromIntegral ms) * (velY $ physicalVel po)
 
-      applyAcc po = po { physicalVel = (Velocity vX vY) } ---TODO see above
-        where
-          vX = (velX $ physicalVel po) + (fromIntegral ms) * (accX $ physicalAcc po)
-          vY = (velY $ physicalVel po) + (fromIntegral ms) * (accY $ physicalAcc po)
+    applyAcc po = po { physicalVel = (Velocity vX vY) } ---TODO see above
+      where
+        vX = (velX $ physicalVel po) + (fromIntegral ms) * (accX $ physicalAcc po)
+        vY = (velY $ physicalVel po) + (fromIntegral ms) * (accY $ physicalAcc po)
 
-      applyRotVel po = po { physicalRot = fixedRot } ---TODO see above
-        where
-          fixedRot | newRot > 2.0 * pi = newRot - 2.0 * pi
-                   | newRot < 0        = newRot + 2.0 * pi
-                   | otherwise         = newRot
-          newRot = (physicalRot po)  + dRot
-          dRot = (fromIntegral ms) * (physicalRotSpeed po)
+    applyRotVel po = po { physicalRot = fixedRot } ---TODO see above
+      where
+        fixedRot | newRot > 2.0 * pi = newRot - 2.0 * pi
+                 | newRot < 0        = newRot + 2.0 * pi
+                 | otherwise         = newRot
+        newRot = (physicalRot po)  + dRot
+        dRot = (fromIntegral ms) * (physicalRotSpeed po)
 
-      applyRotAcc po = po { physicalRotSpeed = newRotSpeed } ---TODO see above
-        where
-          newRotSpeed = (physicalRotAcceleration po) + dRotSpeed
-          dRotSpeed = (fromIntegral ms) * (physicalRotAcceleration po)
+    applyRotAcc po = po { physicalRotSpeed = newRotSpeed } ---TODO see above
+      where
+        newRotSpeed = (physicalRotAcceleration po) + dRotSpeed
+        dRotSpeed = (fromIntegral ms) * (physicalRotAcceleration po)
 
-      applyDrag = id ---TODO
-      applyRotDrag = id ---TODO
+    applyDrag = id ---TODO
+    applyRotDrag = id ---TODO
