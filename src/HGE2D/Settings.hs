2be425de803956d8ab90cab6020f8534233f762d
Martin Buck <buckmartin@buckmartin.de>
2016-07-10 13:54:15 +0200

    docs for Settings

--- src/HGE2D/Settings.hs
+++ src/HGE2D/Settings.hs
@@ -1,5 +1,15 @@
+-- |
+-- Module      :  HGE2D.Settings
+-- Copyright   :  (c) 2016 Martin Buck
+-- License     :  see LICENSE
+--
+-- Containing global settings used by the engine
+
 module HGE2D.Settings where
 
 ---TODO define via class?
+---TODO unused?
+
+-- | Number of faces a circle should be created with
 nFacesCircle :: Int
 nFacesCircle = 100
