37697bd5b8aff33df735f70aeb8b9a154aec02b4
Martin Buck <buckmartin@buckmartin.de>
2016-07-10 12:25:29 +0200

    docs for Classes

--- src/HGE2D/Classes.hs
+++ src/HGE2D/Classes.hs
@@ -1,4 +1,12 @@
+-- |
+-- Module      :  HGE2D.Classes
+-- Copyright   :  (c) 2016 Martin Buck
+-- License     :  see LICENSE
+--
+-- Containing class definitions used within HGE2D
+
 {-# LANGUAGE ConstraintKinds #-}
+
 module HGE2D.Classes where
 
 import HGE2D.Types
@@ -6,32 +14,39 @@ import HGE2D.Datas
 
 --------------------------------------------------------------------------------
 
+-- | For types which are affected by time
 class Dynamic a where
     moveInTime :: Millisecond -> a -> a
 
 --------------------------------------------------------------------------------
 
+-- | For types than can be directly rendered to GL (only required by the engine)
+--   Use GlInstructable to define your rendereble types
 class GlRender a where
     glRender :: a -> IO ()
 
 --------------------------------------------------------------------------------
 
+-- | For types which can be turned into render instructions to be rendered by the engine
 class GlInstructable a where
     toGlInstruction :: a -> RenderInstruction
 
 --------------------------------------------------------------------------------
 
+-- | For types which have a bounding box
 class HasBoundingBox a where
     getBB :: a -> BoundingBox
 
 --------------------------------------------------------------------------------
 
+-- | For types which have / are a physical object
 class IsPhysicalObject a where
     getPhys :: a -> PhysicalObject
     setPhys :: PhysicalObject -> a -> a
 
 --------------------------------------------------------------------------------
 
+-- | For types which are positioned in space
 class Positioned a where
     getPos :: a -> RealPosition
     getX   :: a -> Double
@@ -39,12 +54,14 @@ class Positioned a where
 
 --------------------------------------------------------------------------------
 
+-- | For types which can be moved in space
 class Moveable a where
     moveBy :: RealPosition -> a -> a
     moveTo :: RealPosition -> a -> a
 
 --------------------------------------------------------------------------------
 
+-- | For types which can be accelerated
 class Acceleratable a where
     accBy :: Velocity -> a -> a
     accTo :: Velocity -> a -> a
