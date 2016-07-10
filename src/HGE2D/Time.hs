57e88303b85067283a978499c520cd16fcafd393
Martin Buck <buckmartin@buckmartin.de>
2016-07-10 13:25:45 +0200

    added docs for Time

--- src/HGE2D/Time.hs
+++ src/HGE2D/Time.hs
@@ -1,3 +1,10 @@
+-- |
+-- Module      :  HGE2D.Time
+-- Copyright   :  (c) 2016 Martin Buck
+-- License     :  see LICENSE
+--
+-- Containing functions to get the current time or transform times
+
 module HGE2D.Time where
 
 import HGE2D.Types
@@ -6,10 +13,12 @@ import Data.Time.Clock
 
 --------------------------------------------------------------------------------
 
+-- | Get the current time in seconds
 getSeconds :: IO Double
 getSeconds = getCurrentTime >>= return . fromRational . toRational . utctDayTime
 
 --------------------------------------------------------------------------------
 
+-- | Transform seconds to milliseconds
 toMilliSeconds :: Double -> Millisecond
 toMilliSeconds secs = floor $ 1000 * secs
