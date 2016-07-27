0d8c98b44f46f507a1b7fb03defd8b6581de1207
Martin Buck <buckmartin@buckmartin.de>
2016-07-27 22:01:33 +0200

    added first methods of Geometry.hs to the tests

--- src/tests/Test.hs
+++ src/tests/Test.hs
@@ -1,6 +1,10 @@
+---TODO copy
+---TODO module
+
 import HGE2D.Types
 import HGE2D.Classes
 import HGE2D.Instances
+import HGE2D.Geometry
 import HGE2D.Time
 
 import Test.Hspec
@@ -10,10 +14,16 @@ import Control.Exception (evaluate)
 main :: IO ()
 main = hspec $ do
     describe "Time.hs" $ do
-        it "transforms a floating point seconds into integer milliseconds" $ do
+        it "toMilliSeconds" $ do
             toMilliSeconds 0.73     `shouldBe`      (730 :: Int)
             toMilliSeconds 0.0      `shouldBe`      (0 :: Int)
             toMilliSeconds (-10.0)  `shouldBe`      (-10000 :: Int)
+
+    describe "Geometry.hs" $ do
+        it "rad2deg" $
+            property $ \ x -> x * 180.0 / pi    == rad2deg ( x :: Double)
+        it "deg2rad" $
+            property $ \x -> x * pi / 180.0     == deg2rad ( x :: Double)
 
     describe "RealPosition" $ do
         it "Positioned getX" $
