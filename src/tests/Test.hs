9f42663ebfb6ff67beccbcecd3272cce76061653
Martin Buck <buckmartin@buckmartin.de>
2016-07-27 21:57:53 +0200

    added basic RealPosition tests

--- src/tests/Test.hs
+++ src/tests/Test.hs
@@ -1,3 +1,6 @@
+import HGE2D.Types
+import HGE2D.Classes
+import HGE2D.Instances
 import HGE2D.Time
 
 import Test.Hspec
@@ -11,3 +14,11 @@ main = hspec $ do
             toMilliSeconds 0.73     `shouldBe`      (730 :: Int)
             toMilliSeconds 0.0      `shouldBe`      (0 :: Int)
             toMilliSeconds (-10.0)  `shouldBe`      (-10000 :: Int)
+
+    describe "RealPosition" $ do
+        it "Positioned getX" $
+            property $ \ x y -> fst (x,y)       == getX   ( (x,y) :: RealPosition )
+        it "Positioned getY" $
+            property $ \ x y -> snd (x,y)       == getY   ( (x,y) :: RealPosition )
+        it "Positioned getPos" $
+            property $ \ x y ->     (x,y)       == getPos ( (x,y) :: RealPosition )
