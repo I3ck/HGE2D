4e5e1337cee2aa51bac02281e1bfba5ad575489a
Martin Buck <buckmartin@buckmartin.de>
2016-07-27 21:46:32 +0200

    added test for time

--- src/tests/Test.hs
+++ src/tests/Test.hs
@@ -1,15 +1,13 @@
+import HGE2D.Time
+
 import Test.Hspec
 import Test.QuickCheck
 import Control.Exception (evaluate)
 
 main :: IO ()
 main = hspec $ do
-  describe "Prelude.head" $ do
-    it "returns the first element of a list" $ do
-      head [23 ..] `shouldBe` (23 :: Int)
-
-    it "returns the first element of an *arbitrary* list" $
-      property $ \x xs -> head (x:xs) == (x :: Int)
-
-    it "throws an exception if used with an empty list" $ do
-      evaluate (head []) `shouldThrow` anyException
+    describe "Time.hs" $ do
+        it "transforms a floating point seconds into integer milliseconds" $ do
+            toMilliSeconds 0.73     `shouldBe`      (730 :: Int)
+            toMilliSeconds 0.0      `shouldBe`      (0 :: Int)
+            toMilliSeconds (-10.0)  `shouldBe`      (-10000 :: Int)
